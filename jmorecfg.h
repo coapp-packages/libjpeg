a8186fcacdf56e4f9a43bffc9f710ef1830fc204
HamishC <hamish.cumming@gmail.com>
2012-04-01 16:27:41 +1200

    added package files, added extra vs project to create dll

--- jmorecfg.h
+++ jmorecfg.h
@@ -194,7 +194,11 @@ typedef unsigned int JDIMENSION;
 /* a function referenced thru EXTERNs: */
 #define GLOBAL(type)		type
 /* a reference to a GLOBAL function: */
-#define EXTERN(type)		extern type
+#ifdef JPEG_DLL
+  #define EXTERN(type)		__declspec( dllexport ) type
+#else
+  #define EXTERN(type)		extern type
+#endif
 
 
 /* This macro is used to declare a "method", that is, a function pointer.
